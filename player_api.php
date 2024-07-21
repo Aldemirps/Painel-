0200e5e0275fb4e883b01cd3be1b039426a93113
Aldemirps <161108541+Aldemirps@users.noreply.github.com>
2024-07-21 16:05:38 -0300

    Add files via upload

---
+++ player_api.php
@@ -0,0 +1,32 @@
+{
+  "user_info": {
+    "username": "ccf3050a32ea",
+    "password": "ccf3050a32ea",
+    "message": "Welcome to XUI.one",
+    "auth": 1,
+    "status": "Active",
+    "exp_date": "1735655400",
+    "is_trial": "0",
+    "active_cons": 0,
+    "created_at": "1719597219",
+    "max_connections": "1",
+    "allowed_output_formats": [
+      "m3u8",
+      "ts",
+      "rtmp"
+    ]
+  },
+  "server_info": {
+    "xui": true,
+    "version": "1.5.12",
+    "revision": 2,
+    "url": "go.suafonte.pro",
+    "port": "80",
+    "https_port": "443",
+    "server_protocol": "http",
+    "rtmp_port": "8880",
+    "timestamp_now": 1721585522,
+    "time_now": "2024-07-21 15:12:02",
+    "timezone": "America/Sao_Paulo"
+  }
+}
